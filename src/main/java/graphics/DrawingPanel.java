0de6f0618eb4833c2a02c1a9f6de647b133d7d5d
김영서 <oseo0202@gim-yeongseoui-MacBookPro.local>
2022-04-11 21:36:27 +0900

    added MouseHandler to DrawingPanel

--- src/main/java/graphics/DrawingPanel.java
+++ src/main/java/graphics/DrawingPanel.java
@@ -1,7 +1,53 @@
 package graphics;
 
 import javax.swing.JPanel;
+import java.awt.Graphics;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 
 public class DrawingPanel extends JPanel {
+
+    public DrawingPanel(){
+
+        MouseHandler handler = new MouseHandler();
+		this.addMouseListener(handler);
+		this.addMouseMotionListener(handler);
+		this.addMouseWheelListener(handler);
+    }
+
+    public void paint(Graphics graphics){
+        super.paint(graphics);
+    }
+
+    private class MouseHandler implements MouseListener, MouseMotionListener, MouseWheelListener{
+
+        public void mouseClicked(MouseEvent e){
+		}
+		
+		public void mouseMoved(MouseEvent e){
+		}
+		
+		public void mousePressed(MouseEvent e) {
+		}
+
+		public void mouseReleased(MouseEvent e) {
+		}
+
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		public void mouseExited(MouseEvent e) {
+		}
+
+		public void mouseDragged(MouseEvent e) {
+		}
+	
+		public void mouseWheelMoved(MouseWheelEvent e) {
+		}
+    }
     
 }
