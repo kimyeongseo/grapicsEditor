2bed81254b8d43f3c4ba303562a164e2e7bed1f7
김영서 <oseo0202@gim-yeongseoui-MacBookPro.local>
2022-04-11 21:22:35 +0900

    ToolBar Button에 Icon 적용하기

--- src/main/java/graphics/ToolBar.java
+++ src/main/java/graphics/ToolBar.java
@@ -1,6 +1,8 @@
 package graphics;
 
 import javax.swing.ButtonGroup;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JRadioButton;
 import javax.swing.JToolBar;
 
@@ -11,25 +13,34 @@ public class ToolBar extends JToolBar {
     private JRadioButton polygonTool;
     private JRadioButton lineTool;
 
+    private Icon rectangleIcon;
+	private Icon ovalIcon;
+	private Icon lineIcon;
+	private Icon polygonIcon;
+
     public ToolBar(){
         ButtonGroup buttonGroup = new ButtonGroup();
 
-        this.rectangleTool = new JRadioButton("rectangle");
-        this.add(this.rectangleTool);
-        buttonGroup.add(this.rectangleTool);
+        this.rectangleIcon = new ImageIcon("Icon/icons8-rectangle-24.png");
+		this.rectangleTool = new JRadioButton(rectangleIcon);
+		this.add(this.rectangleTool);
+		buttonGroup.add(this.rectangleTool);
+		
+		this.ovalIcon = new ImageIcon("Icon/icons8-oval-24.png");
+		this.ovalTool = new JRadioButton(ovalIcon);
+		this.add(this.ovalTool);
+		buttonGroup.add(this.ovalTool);
+		
+		this.polygonIcon = new ImageIcon("Icon/icons8-polygon-24.png");
+		this.polygonTool = new JRadioButton(polygonIcon);
+		this.add(this.polygonTool);	
+		buttonGroup.add(this.polygonTool);
 
-        this.ovalTool = new JRadioButton("oval");
-        this.add(this.ovalTool);
-        buttonGroup.add(this.ovalTool);
-
-        this.polygonTool = new JRadioButton("polygon");
-        this.add(this.polygonTool);
-        buttonGroup.add(this.polygonTool);
-
-        this.lineTool = new JRadioButton("line");
-        this.add(this.lineTool);
-        buttonGroup.add(this.lineTool);
-
+        this.lineIcon = new ImageIcon("Icon/icons8-line-24.png");
+		this.lineTool = new JRadioButton(lineIcon);
+		this.add(this.lineTool);
+		buttonGroup.add(this.lineTool);
+		
     }
 
 }
