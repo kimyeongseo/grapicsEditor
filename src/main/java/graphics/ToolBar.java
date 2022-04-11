efd98c91bf22c09ca74253e3034ba8d5495cd67f
김영서 <oseo0202@gim-yeongseoui-MacBookPro.local>
2022-04-11 21:17:00 +0900

    JRadioButton을 사용해 ToolBar Tool Buttons 만들기

--- src/main/java/graphics/ToolBar.java
+++ src/main/java/graphics/ToolBar.java
@@ -1,7 +1,35 @@
 package graphics;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
 import javax.swing.JToolBar;
 
 public class ToolBar extends JToolBar {
     
+    private JRadioButton rectangleTool;
+    private JRadioButton ovalTool;
+    private JRadioButton polygonTool;
+    private JRadioButton lineTool;
+
+    public ToolBar(){
+        ButtonGroup buttonGroup = new ButtonGroup();
+
+        this.rectangleTool = new JRadioButton("rectangle");
+        this.add(this.rectangleTool);
+        buttonGroup.add(this.rectangleTool);
+
+        this.ovalTool = new JRadioButton("oval");
+        this.add(this.ovalTool);
+        buttonGroup.add(this.ovalTool);
+
+        this.polygonTool = new JRadioButton("polygon");
+        this.add(this.polygonTool);
+        buttonGroup.add(this.polygonTool);
+
+        this.lineTool = new JRadioButton("line");
+        this.add(this.lineTool);
+        buttonGroup.add(this.lineTool);
+
+    }
+
 }
