c537c8f6139de0c944f17c19245db57adf307df6
김영서 <oseo0202@gim-yeongseoui-MacBookPro.local>
2022-04-11 21:15:43 +0900

    layoutManager를 사용하여 graphics editor 화면 구성하기

--- src/main/java/graphics/MainFrame.java
+++ src/main/java/graphics/MainFrame.java
@@ -1,6 +1,7 @@
 package graphics;
 
 import javax.swing.JFrame;
+import java.awt.BorderLayout;
 
 public class MainFrame extends JFrame {
     
@@ -13,13 +14,16 @@ public class MainFrame extends JFrame {
 		this.setSize(800, 600);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 윈도우 닫으면 프로세스 종료 
 
+        BorderLayout layoutManager = new BorderLayout();
+		this.setLayout(layoutManager);
+
         this.menuBar = new MenuBar();
         this.setJMenuBar(this.menuBar);
 
         this.toolBar = new ToolBar();
-        this.add(this.toolBar);
+        this.add(this.toolBar, layoutManager.NORTH);
 
         this.drawingPanel = new DrawingPanel();
-        this.add(this.drawingPanel);
+        this.add(this.drawingPanel, layoutManager.CENTER);
     }
 }
